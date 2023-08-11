1ff138ff0525ebf1ba9a84a6b64ebfc77d2ffb65
EslamElnady0 <eslamelnady254@gmail.com>
2023-08-12 02:38:24 +0300

    updated search date to be for the last 15 days

--- lib/Data/Reopsitories/all_news_repo.dart
+++ lib/Data/Reopsitories/all_news_repo.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:news_app/Data/Models/all_news/all_news.dart';
+import 'package:news_app/Global/constants.dart';
 
 class AllNewsRepo {
   Future<AllNews?> getAllNews(String query) async {
     try {
       var response = await http.get(Uri.parse(
-          "https://newsapi.org/v2/everything?q=$query&from=2023-07-15&sortBy=publishedAt&apiKey=cb4e6563ff2b457383df79754ad60aae"));
+          "$baseUrl?q=$query&from=$formattedDate&sortBy=publishedAt&apiKey=$apiKey"));
       Map<String, dynamic> decodedResponse = json.decode(response.body);
 
       if (response.statusCode == 200) {
@@ -15,7 +16,7 @@ class AllNewsRepo {
 
         return data;
       } else {
-        print("request failed");
+        print("http request failed");
         return null;
       }
     } catch (e) {
--- lib/Global/constants.dart
+++ lib/Global/constants.dart
@@ -1,2 +1,7 @@
-String baseUrl = "";
-String apiKey = "";
+import 'package:intl/intl.dart';
+
+const baseUrl = "https://newsapi.org/v2/everything";
+const apiKey = "e833c1f80a5b4c96ad48942b7b9f9a72";
+
+DateTime targetDate = DateTime.now().subtract(const Duration(days: 15));
+String formattedDate = DateFormat('yyyy-MM-dd').format(targetDate);
